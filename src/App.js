508f82be1230f697b1e20489fc784f4fa692c5bc
Bill Sutton <btm1547@gmail.com>
2022-04-17 03:30:39 -0400

    updated react message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Created entierly on a Pixel 6
         </p>
         <a
           className="App-link"
